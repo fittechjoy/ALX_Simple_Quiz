845eed083ff9b8cf8b495a1a12a0e66c3139952e
Joy Shayan <kiamao1joy@gmail.com>
2025-08-09 12:10:34 +0300

    Add Updated changes

--- quiz.js
+++ quiz.js
@@ -1,12 +1,26 @@
-function checkAnswer() {
-    const correctAnswer = "4";
-    const userAnswer = document.querySelector('input[name="quiz"]:checked').value;
+// function checkAnswer() {
+//     const correctAnswer = "4";
+//     const userAnswer = document.querySelector('input[name="quiz"]:checked').value;
 
-    if (userAnswer === correctAnswer) {
-        document.getElementById("feedback").textContent = "Correct! Well done.";
+//     if (userAnswer === correctAnswer) {
+//         document.getElementById("feedback").textContent = "Correct! Well done.";
+//     } else {
+//         document.getElementById("feedback").textContent = "That's incorrect. Try again!";
+//     }
+// }
+
+// document.getElementById("submit-answer").addEventListener("click", checkAnswer);
+const button = document.getElementById("submit-answer");
+button.addEventListener("click", checkAnswer);
+
+
+function checkAnswer(){
+    const correctAnswer = "4";
+    const userAnswer = document.querySelector('input[type="radio"]:checked').value;
+
+    if (correctAnswer == userAnswer){
+        document.getElementById("feedback").innerHTML = "Correct! Well done.";
     } else {
-        document.getElementById("feedback").textContent = "That's incorrect. Try again!";
+        document.getElementById("feedback").innerHTML = "That's incorrect. Try again!";
     }
 }
-
-document.getElementById("submit-answer").addEventListener("click", checkAnswer);
